cd30a43c38d669e2aca352ffe61b59c8ed982e00
yammyxing <yammyxing@foxmail.com>
2022-05-14 22:26:02 +0800

    medium deep-readonly

--- src/challenges/medium/00009-medium-deep-readonly/template.ts
+++ src/challenges/medium/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,5 @@
-type DeepReadonly<T> = any
+// recursively
+// keyof Primitive-Type extends never
+type DeepReadonly<T> = {
+	readonly [P in keyof T] : keyof T[P] extends never ? T[P] : DeepReadonly<T[P]>
+}
