807e0a2fc7963b1e6e294d62b4a78c9c0eb2e988
yammyxing <yammyxing@foxmail.com>
2022-05-14 21:51:43 +0800

    medium omit

--- src/challenges/medium/00003-medium-omit/template.ts
+++ src/challenges/medium/00003-medium-omit/template.ts
@@ -1,1 +1,9 @@
-type MyOmit<T, K> = any
+// this test case, K extends keyof T
+type MyOmit<T, K extends keyof T> = {
+	[P in Exclude<keyof T, K>]: T[P]
+}
+
+// real omit, K extends string | number | symbol
+type RealOmit<T, K extends string | number | symbol> = {
+	[P in Exclude<keyof T, K>]: T[P]
+}
