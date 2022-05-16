635287e4398d8828f95fa27e4fee839aedcd7452
yammyxing <yammyxing@foxmail.com>
2022-05-16 20:54:59 +0800

    medium replace

--- src/challenges/medium/00116-medium-replace/template.ts
+++ src/challenges/medium/00116-medium-replace/template.ts
@@ -1,1 +1,1 @@
-type Replace<S extends string, From extends string, To extends string> = any
+type Replace<S extends string, From extends string, To extends string> = From extends '' ? S : S extends `${infer L}${From}${infer R}` ? `${L}${To}${R}` : S;
