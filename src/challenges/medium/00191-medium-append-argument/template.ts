a175fb151994f0455d80f8f675d490a3ac941248
yammyxing <yammyxing@gmail.com>
2022-05-16 21:47:58 +0800

    medium append-argument

--- src/challenges/medium/00191-medium-append-argument/template.ts
+++ src/challenges/medium/00191-medium-append-argument/template.ts
@@ -1,1 +1,4 @@
-type AppendArgument<Fn, A> = any
+type MyParameters<Fn extends (...args: any) => any> = Fn extends (...args: infer P) => any ? P : never;
+type MyReturnType<Fn extends (...args: any) => any> = Fn extends (...args: any) => infer R ? R : any;
+
+type AppendArgument<Fn extends (...args: any) => unknown, A> = (...args: [...MyParameters<Fn>, A]) => MyReturnType<Fn>;
