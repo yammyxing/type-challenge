d34e2ee2f832f9f1091a834bbf1796ea52de8169
yammyxing <yammyxing@gmail.com>
2022-05-19 16:07:54 +0800

    medium remove-index-signature

--- src/challenges/medium/01367-medium-remove-index-signature/template.ts
+++ src/challenges/medium/01367-medium-remove-index-signature/template.ts
@@ -1,1 +1,5 @@
-type RemoveIndexSignature<T> = any
+type Primitive<T, P extends keyof T> = (string | symbol) extends T[P] ? never : P;
+
+type RemoveIndexSignature<T> = {
+    [P in keyof T as Primitive<T, P>]: T[P]
+}
