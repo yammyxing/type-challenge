f75fed21e1ef13bc1735bc2df67bae8456e2d704
yammyxing <yammyxing@gmail.com>
2022-05-24 22:28:15 +0800

    medium flip-arguments

--- src/challenges/medium/03196-medium-flip-arguments/test-cases.ts
+++ src/challenges/medium/03196-medium-flip-arguments/test-cases.ts
@@ -1,5 +1,11 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+
+type P = Parameters<(arg0: string, arg1: number, arg2: boolean) => void>
+type RR = ReturnType<(arg0: string, arg1: number, arg2: boolean) => void>
+type R = Reverse<P>
+type F = FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>
+
 type cases = [
   Expect<Equal<FlipArguments<() => boolean>, () => boolean>>,
   Expect<Equal<FlipArguments<(foo: string) => number>, (foo: string) => number>>,
