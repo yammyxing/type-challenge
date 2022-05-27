196863a2941db10d1192e5edadac9ff0455811e8
yammyxing <yammyxing@gmail.com>
2022-05-27 22:28:31 +0800

    medium bem-style-string

--- src/challenges/medium/03326-medium-bem-style-string/test-cases.ts
+++ src/challenges/medium/03326-medium-bem-style-string/test-cases.ts
@@ -1,5 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type M = BEM<'btn', ['price'], []>
+
 type cases = [
   Expect<Equal<BEM<'btn', ['price'], []>, 'btn__price'>>,
   Expect<Equal<BEM<'btn', ['price'], ['warning', 'success']>, 'btn__price--warning' | 'btn__price--success' >>,
