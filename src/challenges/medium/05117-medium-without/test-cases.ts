125948ac2fe264c3c2daba8f5a346e5d79005b82
yammyxing <yammyxing@gmail.com>
2022-07-12 08:02:28 +0800

    feat: medium without

--- src/challenges/medium/05117-medium-without/test-cases.ts
+++ src/challenges/medium/05117-medium-without/test-cases.ts
@@ -1,5 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type W = Without<[1, 2, 4, 1, 5], [1, 2]>
+
 type cases = [
   Expect<Equal<Without<[1, 2], 1>, [2]>>,
   Expect<Equal<Without<[1, 2, 4, 1, 5], [1, 2]>, [4, 5]>>,
